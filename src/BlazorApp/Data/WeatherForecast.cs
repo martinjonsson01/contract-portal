080c8b5cdf0a5e02bd90951cfde7b1d4ad29c7a2
Martin <martinjonsson01@gmail.com>
2022-03-29 06:10:24 +0200

    Change WeatherForecast.cs to conform to code style

--- src/BlazorApp/Data/WeatherForecast.cs
+++ src/BlazorApp/Data/WeatherForecast.cs
@@ -1,12 +1,27 @@
 namespace BlazorApp.Data;
 
+/// <summary>
+/// Hi there.
+/// </summary>
 public class WeatherForecast
 {
+    /// <summary>
+    /// Gets or sets test.
+    /// </summary>
     public DateTime Date { get; set; }
 
+    /// <summary>
+    /// Gets or sets test.
+    /// </summary>
     public int TemperatureC { get; set; }
 
+    /// <summary>
+    /// Gets test.
+    /// </summary>
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+    /// <summary>
+    /// Gets or sets test.
+    /// </summary>
     public string? Summary { get; set; }
 }
