30021f686ab85d0d0bdd815cf386ddd25b4e3423
Martin <martinjonsson01@gmail.com>
2022-03-29 06:10:00 +0200

    Change Error.cshtml.cs to conform to code style

--- src/BlazorApp/Pages/Error.cshtml.cs
+++ src/BlazorApp/Pages/Error.cshtml.cs
@@ -1,24 +1,43 @@
-﻿using System.Diagnostics;
+﻿namespace BlazorApp.Pages;
+
 using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BlazorApp.Pages;
-
+/// <summary>
+/// Test.
+/// </summary>
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-    public string? RequestId { get; set; }
+    private readonly ILogger<ErrorModel> logger;
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-    private readonly ILogger<ErrorModel> _logger;
-
+    /// <summary>
+    /// Test.
+    /// </summary>
+    /// <param name="logger">What.</param>
     public ErrorModel(ILogger<ErrorModel> logger)
     {
-        _logger = logger;
+        this.logger = logger;
     }
 
+    /// <summary>
+    /// Gets or sets test.
+    /// </summary>
+    public string? RequestId { get; set; }
+
+    /// <summary>
+    /// Gets a value indicating whether test.
+    ///
+    /// </summary>
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    /// <summary>
+    /// TEst.
+    /// </summary>
     public void OnGet()
     {
         RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
