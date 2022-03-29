a0898edf4257d2e471ff92866e14c3c4b92a6f79
Martin <martinjonsson01@gmail.com>
2022-03-29 06:10:32 +0200

    Change WeatherForecastService.cs to conform to code style

--- src/BlazorApp/Data/WeatherForecastService.cs
+++ src/BlazorApp/Data/WeatherForecastService.cs
@@ -1,19 +1,30 @@
 namespace BlazorApp.Data;
 
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>
+/// Test.
+/// </summary>
 public class WeatherForecastService
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
     };
 
+    /// <summary>
+    /// Documentation.
+    /// </summary>
+    /// <param name="startDate">Need text.</param>
+    /// <returns>Ur mome.</returns>
+    [SuppressMessage("Security", "CA5394:Do not use insecure randomness", Justification = "Because ur mum")]
     public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
         return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = startDate.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
         }).ToArray());
     }
 }
