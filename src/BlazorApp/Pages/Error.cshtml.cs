1a4f813a777851eeaaeb6604cf7930b2bdba57ce
Martin <martinjonsson01@gmail.com>
2022-03-30 16:44:53 +0200

    Revert "Hello"
    This reverts commit 3dea70b97787b997f68e526fb89c87416190fb1c.

--- src/BlazorApp/Pages/Error.cshtml.cs
+++ src/BlazorApp/Pages/Error.cshtml.cs
@@ -1,12 +1,13 @@
 ﻿namespace BlazorApp.Pages;
 
+using Microsoft.AspNetCore.Mvc;
+
 using System.Diagnostics;
 
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 /// <summary>
-///     Test.
+/// Test.
 /// </summary>
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
@@ -15,7 +16,7 @@ public class ErrorModel : PageModel
     private readonly ILogger<ErrorModel> logger;
 
     /// <summary>
-    ///     Test.
+    /// Test.
     /// </summary>
     /// <param name="logger">What.</param>
     public ErrorModel(ILogger<ErrorModel> logger)
@@ -24,22 +25,21 @@ public class ErrorModel : PageModel
     }
 
     /// <summary>
-    ///     Gets or sets test.
+    /// Gets or sets test.
     /// </summary>
     public string? RequestId { get; set; }
 
     /// <summary>
-    ///     Gets a value indicating whether test.
+    /// Gets a value indicating whether test.
+    ///
     /// </summary>
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     /// <summary>
-    ///     TEst.
+    /// TEst.
     /// </summary>
     public void OnGet()
     {
-        Console.WriteLine("Hi world!");
-
         RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
 }
