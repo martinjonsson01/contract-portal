216b1bb5b7f1e5d7b58526b26f471101c135abbe
Martin <martinjonsson01@gmail.com>
2022-03-31 21:18:03 +0200

    Make Action fail due to coverage instead of test

--- tests/Example.Tests/UnitTest1.cs
+++ tests/Example.Tests/UnitTest1.cs
@@ -1,10 +1,14 @@
 namespace Example.Tests;
 
+using System;
+
+using BlazorApp.Data;
+
 using Xunit;
 
 public class UnitTest1
 {
-    [Fact]
+    [Fact(Skip = "Disabled for coverage testing")]
     public void Test_WillAlways_Fail()
     {
         Assert.True(false);
@@ -13,6 +17,8 @@ public class UnitTest1
     [Fact]
     public void Test_WillAlways_Succeed()
     {
+        var forecast = new WeatherForecast();
+        Console.WriteLine(forecast.Date);
         Assert.True(true);
     }
 }
