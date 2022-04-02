246f71f8e8749a179a6c939c51713248e6e6b315
Martin <martinjonsson01@gmail.com>
2022-04-02 15:00:15 +0200

    Reformat example code

--- src/BlazorApp/Pages/Error.cshtml.cs
+++ src/BlazorApp/Pages/Error.cshtml.cs
@@ -1,45 +1,41 @@
-﻿namespace BlazorApp.Pages;
+﻿using System.Diagnostics;
 
 using Microsoft.AspNetCore.Mvc;
-
-using System.Diagnostics;
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace BlazorApp.Pages;
+
 /// <summary>
-/// Test.
+///     Test.
 /// </summary>
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-    private readonly ILogger<ErrorModel> logger;
+    private readonly ILogger<ErrorModel> _logger;
 
     /// <summary>
-    /// Test.
+    ///     Test.
     /// </summary>
     /// <param name="logger">What.</param>
-    public ErrorModel(ILogger<ErrorModel> logger)
-    {
-        this.logger = logger;
-    }
+    public ErrorModel(ILogger<ErrorModel> logger) => _logger = logger;
 
     /// <summary>
-    /// Gets or sets test.
+    ///     Gets or sets test.
     /// </summary>
     public string? RequestId { get; set; }
 
     /// <summary>
-    /// Gets a value indicating whether test.
-    ///
+    ///     Gets a value indicating whether test.
     /// </summary>
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     /// <summary>
-    /// TEst.
+    ///     TEst.
     /// </summary>
     public void OnGet()
     {
+        _logger.LogInformation("something happened");
         RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
 }
