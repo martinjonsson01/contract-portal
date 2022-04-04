64f98ce537e72d62beb004a79ae826fc17485bab
Martin <martinjonsson01@gmail.com>
2022-04-04 16:30:51 +0200

    Add example client-server communication
    (Currently not working due to CORS)
    
    Co-authored-by: Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
    Co-authored-by: Mathias Prétot <mathias.pretot@gmail.com>

---
+++ src/Server/Controllers/CounterController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("api/counter123")]
+public class CounterController : Controller
+{
+    [HttpPost]
+    public ActionResult<string> IncrementCounter()
+    {
+        return "hi";
+    }
+}
