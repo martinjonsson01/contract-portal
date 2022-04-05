cb69a4c9c29e96d57092cb6395e7712674aecf78
Martin <martinjonsson01@gmail.com>
2022-04-05 17:00:00 +0200

    Add UI unit test

---
+++ tests/Client.Tests/Pages/FetchDataTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.Metrics;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+
+using Bunit;
+
+using Client.Pages;
+
+using Domain;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Moq;
+
+using RichardSzalay.MockHttp;
+
+using Xunit;
+
+namespace Client.Tests.Pages;
+
+public class FetchDataTests
+{
+    [Fact]
+    public void CounterShouldIncrementWhenClicked()
+    {
+        // Arrange: render the Counter.razor component
+        using var ctx = new TestContext();
+        MockHttpMessageHandler mockHttp = ctx.Services.AddMockHttpClient();
+        mockHttp.When("/WeatherForecast").RespondJson(new WeatherForecast[1]);
+        IRenderedComponent<FetchData> cut = ctx.RenderComponent<FetchData>();
+
+        // Assert: first find the <p> element, then verify its content
+        cut.Find("p em").TextContent.Should().BeEquivalentTo("Loading...");
+    }
+}
