b9fdc0f1399cd2f155f87c374eb3a0cdca5fda35
Martin <martinjonsson01@gmail.com>
2022-04-05 19:03:30 +0200

    Create global usings for Infrastructure.Tests

---
+++ tests/Infrastructure.Tests/GlobalUsings.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable RedundantUsingDirective
+
+#pragma warning disable IDE0005
+global using System.Collections.Generic;
+
+global using Bogus;
+
+global using Domain;
+
+global using FluentAssertions;
+
+global using Moq;
+
+global using Xunit;
+
+#pragma warning restore IDE0005
