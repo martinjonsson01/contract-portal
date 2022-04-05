2ff9d630edaa9b7369a537d342514c85e913f608
Martin <martinjonsson01@gmail.com>
2022-04-05 16:23:16 +0200

    Create example unit test

---
+++ tests/Application.Tests/Weather/WeatherServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Application.Weather;
+
+using Bogus;
+
+using Domain;
+
+using FluentAssertions;
+
+using Moq;
+
+using Xunit;
+
+namespace Application.Tests.Weather;
+
+public class WeatherServiceTests
+{
+    [Fact]
+    public void FetchLatestWeather_ReturnsNonEmpty_WhenXXXX()
+    {
+        // Arrange
+        var mockRepo = new Mock<IWeatherRepository>();
+        List<WeatherForecast> mockWeathers = new Faker<WeatherForecast>().Generate(10);
+        mockRepo.Setup(repo => repo.Forecasts).Returns(mockWeathers);
+        var cut = new WeatherService(mockRepo.Object);
+
+        // Act
+        IEnumerable<WeatherForecast> forcasts = cut.FetchLatestWeather();
+
+        // Assert
+        forcasts.Should().HaveCount(2);
+    }
+}
