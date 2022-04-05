acf7cb6d1a1cb91a48937fa82be5023da4d22729
Martin <martinjonsson01@gmail.com>
2022-04-05 19:38:37 +0200

    Remove unnecessary usings

--- tests/Client.Tests/ServiceExtensions.cs
+++ tests/Client.Tests/ServiceExtensions.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
-using Bunit;
-
 using Microsoft.Extensions.DependencyInjection;
-
-using RichardSzalay.MockHttp;
 
 namespace Client.Tests;
 
