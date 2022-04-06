593f24a20392f4392dc3032bbb587a1622edecd5
Mathias <mathias.pretot@gmail.com>
2022-04-06 14:47:46 +0200

    Add test for ContractService and removed old tests
    Co-authored-by: Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>

---
+++ tests/Application.Tests/Contracts/ContractServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Application.Contracts;
+using Domain.Contracts;
+
+namespace Application.Tests.Contracts;
+
+public class ContractServiceTests
+{
+    [Fact]
+    public void FetchAllContracts_ReturnsALlContractsInTheDatabase()
+    {
+        // Arrange
+        const int numberOfContracts = 10;
+        var mockRepo = new Mock<IContractRepository>();
+        List<Contract> mockContracts = new Faker<Contract>().Generate(numberOfContracts);
+        mockRepo.Setup(repository => repository.Contracts).Returns(mockContracts);
+        var cut = new ContractService(mockRepo.Object);
+
+        // Act
+        IEnumerable<Contract> contracts = cut.FetchAllContracts();
+
+        // Assert
+        contracts.Should().HaveCount(numberOfContracts);
+    }
+}
