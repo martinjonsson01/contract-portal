1006672eb633d6f39ba59d270267d297b5c2c5de
Mathias <mathias.pretot@gmail.com>
2022-04-06 16:35:19 +0200

    Add tests for ContractCard
    Co-authored-by: Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>

---
+++ tests/Client.Tests/Pages/ContractCardTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Client.Tests.Pages;
+
+public class ContractCardTests : IDisposable
+{
+    private readonly TestContext _context;
+
+    public ContractCardTests()
+    {
+        _context = new TestContext();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    [Fact]
+    public void ContractCard_ContainsGivenInformation()
+    {
+        // Arrange
+        IRenderedComponent<ContractCard> cut =
+            _context.RenderComponent<ContractCard>(parameters =>
+                parameters.Add(property => property.Name, "Sj")
+                    .Add(property => property.ImagePath, "/img/test"));
+
+        // Act
+
+        // Assert
+        cut.Find("h3").TextContent.Should().BeEquivalentTo("SJ");
+        cut.Find("img").OuterHtml.Should().Contain("/img/test");
+    }
+}
