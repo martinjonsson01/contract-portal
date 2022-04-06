6233690be9139e52427c1200dd1fee4e56c0351c
Mathias <mathias.pretot@gmail.com>
2022-04-06 16:40:46 +0200

    Add ContractsController tests and removed old example
    Co-authored-by: Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>

---
+++ tests/Server.Tests/Controllers/ContractsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Application.Contracts;
+using Domain.Contracts;
+
+namespace Server.Tests.Controllers;
+
+public class ContractsControllerTests : System.IDisposable
+{
+    private readonly ContractsController _cut;
+    private readonly Mock<IContractService> _mockContracts;
+
+    public ContractsControllerTests()
+    {
+        _mockContracts = new Mock<IContractService>();
+        _cut = new ContractsController(_mockContracts.Object);
+    }
+
+    [Fact]
+    public void Get_AllContracts()
+    {
+        // Arrange
+        List<Contract>? fakeContracts = new Faker<Contract>().Generate(10);
+        _mockContracts.Setup(service => service.FetchAllContracts()).Returns(fakeContracts);
+
+        // Act
+        IEnumerable<Contract> actualWeather = _cut.AllContracts();
+
+        // Assert
+        actualWeather.Should().BeEquivalentTo(fakeContracts);
+    }
+
+    public void Dispose()
+    {
+        _cut.Dispose();
+    }
+}
