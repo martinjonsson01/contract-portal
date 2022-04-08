3aea0100c80a91622c5aeeb92f74ea5fb8b04203
Martin <martinjonsson01@gmail.com>
2022-04-08 07:00:38 +0200

    Add navmenu test

---
+++ tests/Client.Tests/BlazorTestFixture.cs
@@ -0,0 +1,20 @@
+﻿namespace Client.Tests;
+
+public abstract class BlazorTestFixture : IDisposable
+{
+    protected BlazorTestFixture()
+    {
+        Context = new TestContext();
+        MockHttp = Context.Services.AddMockHttpClient();
+    }
+
+    protected TestContext Context { get; }
+
+    protected MockHttpMessageHandler MockHttp { get; }
+
+    public void Dispose()
+    {
+        Context.Dispose();
+        MockHttp.Dispose();
+    }
+}
---
+++ tests/Client.Tests/Shared/NavMenuTests.cs
@@ -0,0 +1,19 @@
+﻿using Client.Shared;
+
+namespace Client.Tests.Shared;
+
+public class NavMenuTests : BlazorTestFixture
+{
+    [Fact]
+    public void ToggleNavMenu_ShowsNavBar_WhenClicked()
+    {
+        // Arrange
+        IRenderedComponent<NavMenu> cut = Context.RenderComponent<NavMenu>();
+
+        // Act
+        cut.Find(".navbar-toggler").Click();
+
+        // Assert
+        cut.FindAll(".nav-item").Count.Should().BePositive();
+    }
+}
