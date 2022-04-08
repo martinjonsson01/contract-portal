69b263cb460d34dae2583136eb9a52e478930d99
Martin <martinjonsson01@gmail.com>
2022-04-08 06:41:40 +0200

    Exclude generated code from coverage

--- coverage.bat
+++ coverage.bat
@@ -1,4 +1,4 @@
-﻿
-dotnet test --verbosity=minimal -p:CollectCoverage=true -p:Threshold=80 -p:ThresholdType=branch -p:CoverletOutputFormat=cobertura
+
+dotnet test --verbosity=minimal -p:CollectCoverage=true -p:Threshold=80 -p:ThresholdType=branch -p:CoverletOutputFormat=cobertura -p:ExcludeByFile=\"**/Program.cs\" -p:ExcludeByAttribute=\"Obsolete,GeneratedCodeAttribute,CompilerGeneratedAttribute\"
 dotnet tool install -g dotnet-reportgenerator-globaltool
 reportgenerator -reports:"tests/Application.Tests/coverage.cobertura.xml;tests/Client.Tests/coverage.cobertura.xml;tests/Domain.Tests/coverage.cobertura.xml;tests/Infrastructure.Tests/coverage.cobertura.xml;tests/Server.Tests/coverage.cobertura.xml" -targetdir:"Coverage/"
