581a3032cb40aa9dd9d10ddaac9bfb646621bcb8
Martin <martinjonsson01@gmail.com>
2022-04-08 06:22:51 +0200

    Create coverage report generation tool

---
+++ coverage.bat
@@ -0,0 +1,4 @@
+﻿
+dotnet test --verbosity=minimal -p:CollectCoverage=true -p:Threshold=80 -p:ThresholdType=branch -p:CoverletOutputFormat=cobertura
+dotnet tool install -g dotnet-reportgenerator-globaltool
+reportgenerator -reports:"tests/Application.Tests/coverage.cobertura.xml;tests/Client.Tests/coverage.cobertura.xml;tests/Domain.Tests/coverage.cobertura.xml;tests/Infrastructure.Tests/coverage.cobertura.xml;tests/Server.Tests/coverage.cobertura.xml" -targetdir:"Coverage/"
