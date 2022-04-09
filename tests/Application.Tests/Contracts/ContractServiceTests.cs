abf54092adde0e1c7908bc4f7d929a879c6d0b57
Martin <martinjonsson01@gmail.com>
2022-04-09 14:59:14 +0200

    Add contract service test

--- tests/Application.Tests/Contracts/ContractServiceTests.cs
+++ tests/Application.Tests/Contracts/ContractServiceTests.cs
@@ -1,24 +1,62 @@
-﻿using Application.Contracts;
+﻿using System;
+
+using Application.Contracts;
+using Application.Exceptions;
+
 using Domain.Contracts;
 
 namespace Application.Tests.Contracts;
 
 public class ContractServiceTests
 {
+    private readonly ContractService _cut;
+    private readonly Mock<IContractRepository> _mockRepo;
+
+    public ContractServiceTests()
+    {
+        _mockRepo = new Mock<IContractRepository>();
+        _cut = new ContractService(_mockRepo.Object);
+    }
+
     [Fact]
     public void FetchAllContracts_ReturnsAllContractsInTheDatabase()
     {
         // Arrange
         const int numberOfContracts = 10;
-        var mockRepo = new Mock<IContractRepository>();
         List<Contract> mockContracts = new Faker<Contract>().Generate(numberOfContracts);
-        mockRepo.Setup(repository => repository.Contracts).Returns(mockContracts);
-        var cut = new ContractService(mockRepo.Object);
+        _mockRepo.Setup(repository => repository.Contracts).Returns(mockContracts);
 
         // Act
-        IEnumerable<Contract> contracts = cut.FetchAllContracts();
+        IEnumerable<Contract> contracts = _cut.FetchAllContracts();
 
         // Assert
         contracts.Should().HaveCount(numberOfContracts);
+    }
+
+    [Fact]
+    public void AddingContract_ThrowsIDException_IfIDAlreadyTaken()
+    {
+        // Arrange
+        var contract = new Contract();
+        _mockRepo.Setup(repository => repository.Contracts).Returns(new[] { contract, });
+
+        // Act
+        Action add = () => _cut.Add(contract);
+
+        // Assert
+        add.Should().Throw<IdentifierAlreadyTakenException>();
+    }
+
+    [Fact]
+    public void AddingContract_DoesNotThrow_IfIDIsUnique()
+    {
+        // Arrange
+        _mockRepo.Setup(repository => repository.Contracts).Returns(new List<Contract>());
+
+        // Act
+        Action add = () => _cut.Add(new Contract());
+
+        // Assert
+        add.Should().NotThrow();
     }
 }
