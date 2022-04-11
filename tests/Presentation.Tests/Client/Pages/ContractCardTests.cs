e696c935bdd2e0c92e2938663f13f64e0dd14e31
Martin <martinjonsson01@gmail.com>
2022-04-11 18:55:35 +0200

    Remove tests of JS code
    Co-authored-by: Emil Nilsson <eminil2@hotmail.com>
    Co-authored-by: Tamás Nagy <tamas_nagy@hotmail.com>

--- tests/Presentation.Tests/Client/Pages/ContractCardTests.cs
+++ tests/Presentation.Tests/Client/Pages/ContractCardTests.cs
@@ -21,30 +21,4 @@ public class ContractCardTests : UITestFixture
         cut.Find("#contract-name").TextContent.Should().Contain(name);
         cut.Find("#contract-thumbnail").OuterHtml.Should().Contain(path);
     }
-
-    [Fact]
-    public void ContractCard_ModalShown_WhenClicked()
-    {
-        // Arrange
-        IRenderedComponent<ContractCard> cut = Context.RenderComponent<ContractCard>();
-
-        // Act
-        cut.Find(".card").Click();
-
-        // Assert
-        cut.Find(".modal.show").Should().NotBeNull();
-    }
-
-    [Fact]
-    public void ContractCard_ModalNotShown_WhenNotClicked()
-    {
-        // Arrange
-        IRenderedComponent<ContractCard> cut = Context.RenderComponent<ContractCard>();
-
-        // Act
-        Action act = () => cut.Find(".modal.show");
-
-        // Assert
-        act.Should().Throw<ElementNotFoundException>();
-    }
 }
