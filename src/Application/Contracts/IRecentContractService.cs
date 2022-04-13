0e701481781605f29544eaeebf31c576f7a6c5bd
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-04-13 13:23:46 +0200

    Create new recent contract service

---
+++ src/Application/Contracts/IRecentContractService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Contracts;
+
+namespace Application.Contracts;
+
+/// <summary>
+/// Methods for recent contracts.
+/// </summary>
+public interface IRecentContractService
+{
+    /// <summary>
+    /// Ensures that a new contract is qualified as recently viewed.
+    /// </summary>
+    /// <param name="contract">Possible new recent contract.</param>
+    public void FilterRecentContracts(Contract contract);
+}
---
+++ src/Application/Contracts/RecentContractService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+using Domain.Contracts;
+
+namespace Application.Contracts;
+
+/// <inheritdoc />
+public class RecentContractService : IRecentContractService
+{
+    private readonly Collection<Contract>? _recent;
+
+    /// <summary>
+    /// Constructs recent contract service.
+    /// </summary>
+    /// <param name="recent">The list of recent contracts.</param>
+    public RecentContractService(Collection<Contract> recent)
+    {
+        _recent = recent;
+    }
+
+    /// <inheritdoc />
+    public void FilterRecentContracts(Contract contract)
+    {
+        if (_recent != null && _recent.Any(recentContract => recentContract.Id.Equals(contract.Id)))
+        {
+            return;
+        }
+
+        const int recentAmountMax = 3;
+        if (_recent != null && _recent.Count >= recentAmountMax)
+        {
+            _recent.RemoveAt(0);
+        }
+
+        _recent?.Add(contract);
+    }
+}
