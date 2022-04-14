8d51e3b172ff1ae3b4ef0559e8f808e8fed02772
Martin <martinjonsson01@gmail.com>
2022-04-14 15:49:56 +0200

    Add example text

--- src/Infrastructure/StatusUpdates/InMemoryStatusUpdateRepository.cs
+++ src/Infrastructure/StatusUpdates/InMemoryStatusUpdateRepository.cs
@@ -11,10 +11,30 @@ public class InMemoryStatusUpdateRepository : IStatusUpdateRepository
 {
     private readonly ICollection<StatusUpdate> _statusUpdates = new List<StatusUpdate>
     {
-        new StatusUpdate { Alert = AlertLevel.Urgent, Timestamp = DateTime.Now.Subtract(TimeSpan.FromDays(12)), },
-        new StatusUpdate { Alert = AlertLevel.Information, Timestamp = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)), },
-        new StatusUpdate { Alert = AlertLevel.Critical, Timestamp = DateTime.Now.Subtract(TimeSpan.FromDays(30 * 3)), },
-        new StatusUpdate { Alert = AlertLevel.Warning, Timestamp = DateTime.Now.Subtract(TimeSpan.FromHours(3)), },
+        new StatusUpdate
+        {
+            Alert = AlertLevel.Urgent,
+            Timestamp = DateTime.Now.Subtract(TimeSpan.FromDays(12)),
+            Text = "En brådskande notis ser ut så här.",
+        },
+        new StatusUpdate
+        {
+            Alert = AlertLevel.Information,
+            Timestamp = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)),
+            Text = "En informationsnotis ser ut så här.",
+        },
+        new StatusUpdate
+        {
+            Alert = AlertLevel.Critical,
+            Timestamp = DateTime.Now.Subtract(TimeSpan.FromDays(30 * 3)),
+            Text = "En kritisk notis ser ut så här.",
+        },
+        new StatusUpdate
+        {
+            Alert = AlertLevel.Warning,
+            Timestamp = DateTime.Now.Subtract(TimeSpan.FromHours(3)),
+            Text = "En varningsnotis ser ut så här.",
+        },
     };
 
     /// <inheritdoc />
