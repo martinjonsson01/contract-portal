c00d7cf74923fe696247b726768094ce0449a54f
Martin <martinjonsson01@gmail.com>
2022-04-20 15:49:47 +0200

    Create NameSearch module
    Co-authored-by: Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>

---
+++ src/Application/Search/Modules/NameSearch.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Contracts;
+
+namespace Application.Search.Modules;
+
+/// <summary>
+/// Searches names of contracts.
+/// </summary>
+public class NameSearch : ISearchModule<Contract>
+{
+    /// <summary>
+    /// Matches a <see cref="Contract"/> name to the given query.
+    /// </summary>
+    /// <param name="entity">The contract.</param>
+    /// <param name="query">The text to match with.</param>
+    /// <returns>Whether the contract matches the name in the query or not.</returns>
+    public bool Match(Contract entity, string query)
+    {
+        return entity.Name.Contains(query, StringComparison.OrdinalIgnoreCase);
+    }
+}
