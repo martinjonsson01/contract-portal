64b99fe9065418f092da741d11dadcc55643fa1d
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-04-21 17:38:53 +0200

    Add supplier name search module and tests

---
+++ src/Application/Search/Modules/SupplierNameSearch.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Contracts;
+
+namespace Application.Search.Modules;
+
+/// <summary>
+/// Searches supplier names of contracts.
+/// </summary>
+public class SupplierNameSearch : ISearchModule<Contract>
+{
+    /// <summary>
+    /// Matches a <see cref="Contract"/> supplier name to the given query.
+    /// </summary>
+    /// <param name="entity">The contract.</param>
+    /// <param name="query">The text to match with.</param>
+    /// <returns>Whether the contract matches the supplier name in query or not.</returns>
+    /// <exception cref="NotImplementedException">not empy.</exception>
+    public bool Match(Contract entity, string query)
+    {
+        return entity.SupplierName.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+               query.Contains(entity.SupplierName, StringComparison.OrdinalIgnoreCase);
+    }
+}
