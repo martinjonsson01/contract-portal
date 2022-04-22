0b76f1ee30a1ca49512da9bb21b3df8e35e8b784
Martin <martinjonsson01@gmail.com>
2022-04-22 20:04:12 +0200

    Make ListInput persist data that is input

--- src/Domain/Contracts/Contract.cs
+++ src/Domain/Contracts/Contract.cs
@@ -38,7 +38,7 @@ public class Contract
     /// <summary>
     ///     Gets the search keywords that have been tagged.
     /// </summary>
-    public IEnumerable<string> Tags { get; init; } = new List<string>();
+    public IList<string> Tags { get; init; } = new List<string>();
 
     /// <summary>
     ///     Gets or sets image to the supplier logo.
