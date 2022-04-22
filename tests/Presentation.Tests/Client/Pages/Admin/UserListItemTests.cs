1e4f1c6e5533099f5b884bdc2d52b31f35cfd740
pkasinski <kasinski@student.chalmers.se>
2022-04-22 17:26:54 +0200

    Create UserListItemTests

---
+++ tests/Presentation.Tests/Client/Pages/Admin/UserListItemTests.cs
@@ -0,0 +1,37 @@
+using Client.Pages.Admin;
+using Domain.Users;
+
+namespace Presentation.Tests.Client.Pages.Admin;
+
+public class UserListItemTests : IDisposable
+{
+    private readonly TestContext _context;
+
+    public UserListItemTests()
+    {
+        _context = new TestContext();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    [Fact]
+    public void UserListItem_ContainsTitle()
+    {
+        // Arrange
+        const string name = "Foo";
+        var user = new User() { Name = name };
+
+        void ParameterBuilder(ComponentParameterCollectionBuilder<UserListItem> parameters) =>
+            parameters.Add(property => property.User, user);
+
+        // Act
+        IRenderedComponent<UserListItem> cut =
+            _context.RenderComponent<UserListItem>(ParameterBuilder);
+
+        // Assert
+        cut.Find($"#{user.Name}").TextContent.Should().Contain(name);
+    }
+}
