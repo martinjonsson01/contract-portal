df15800e73641668c2c280e1e8b5a6cdad9b71f9
pkasinski <kasinski@student.chalmers.se>
2022-04-22 17:06:59 +0200

    Create UserListTests

---
+++ tests/Presentation.Tests/Client/Pages/Admin/UserListTests.cs
@@ -0,0 +1,33 @@
+using System.Linq.Expressions;
+using AngleSharp.Dom;
+using Client.Pages.Admin;
+using Domain.Users;
+
+namespace Presentation.Tests.Client.Pages.Admin;
+
+public class UserListTests : UITestFixture
+{
+    [Fact]
+    public void AddingContract_RendersTheNewContract()
+    {
+        // Arrange
+        User[] users = { new User() { Name = "First", }, new User() { Name = "Second", } };
+        MockHttp.When("/api/v1/users").RespondJson(users);
+
+        IRenderedComponent<UserList> cut = Context.RenderComponent<UserList>();
+        const string itemSelector = ".list-group-item";
+        cut.WaitForElement(itemSelector);
+
+        const string newUserName = "New User";
+        var newUser = new User { Name = newUserName };
+
+        // Act
+        cut.Instance.Add(newUser);
+        cut.WaitForState(() => cut.FindAll(itemSelector).Count == 3);
+
+        // Assert
+        Expression<Func<IElement, bool>>
+            elementWithNewName = contract => contract.TextContent.Contains(newUserName);
+        cut.FindAll(itemSelector).Should().Contain(elementWithNewName);
+    }
+}
