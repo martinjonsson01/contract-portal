94dd49e33e9e88dd08029afdc30be33ce98e2ee5
Martin <martinjonsson01@gmail.com>
2022-04-23 19:16:50 +0200

    Make documentation reflect intent

--- src/Application/Search/Modules/EmptySearch.cs
+++ src/Application/Search/Modules/EmptySearch.cs
@@ -9,7 +9,7 @@ public class EmptySearch<TEntity> : ISearchModule<TEntity>
     /// <summary>
     /// Matches an entity against a query only if it is empty.
     /// </summary>
-    /// <param name="entity">The entity to match against.</param>
+    /// <param name="entity">The entity is completely ignored.</param>
     /// <param name="query">The text input query.</param>
     /// <returns>Whether the query is empty or not.</returns>
     public bool Match(TEntity entity, string query)
