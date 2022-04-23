a7a4b487de5ac092dedab391069aab1d94d50d7a
Martin <martinjonsson01@gmail.com>
2022-04-23 17:07:13 +0200

    Change unnecessarily complex scorer to be simple

---
+++ src/Application/Search/Scorers/IdentityScorer.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Contracts;
+
+namespace Application.Search.Scorers;
+
+/// <summary>
+/// Always scores everything with the multiplicative identity (1).
+/// </summary>
+public class IdentityScorer : IScorer<Contract>
+{
+    /// <summary>
+    /// Scores the given <see cref="Contract"/> a value of 1.
+    /// </summary>
+    /// <param name="entity">The contract to score.</param>
+    /// <param name="query">The query to score against.</param>
+    /// <returns>1 regardless of the input.</returns>
+    public double Score(Contract entity, string query)
+    {
+        return 1d;
+    }
+}
