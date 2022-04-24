097a88d6d48d8775204766bcfcaa94841a29888d
Martin <martinjonsson01@gmail.com>
2022-04-24 11:38:56 +0200

    Rename LINQ variable
    Co-authored-by: Ella Sibbmark <73948350+ellsib@users.noreply.github.com>

--- src/Application/Search/SearchEngine.cs
+++ src/Application/Search/SearchEngine.cs
@@ -43,7 +43,7 @@ public class SearchEngine<TEntity>
         return from entity in entitiesMatchedByModule.Select(tuple => tuple.entity).Distinct()
                let weights = entitiesMatchedByModule.Where(pair => pair.entity.Equals(entity))
                                                     .Select(pair => pair.moduleWeight)
-               let maxWeight = weights.Sum()
+               let totalWeight = weights.Sum()
                select (entity, maxWeight);
     }
 
