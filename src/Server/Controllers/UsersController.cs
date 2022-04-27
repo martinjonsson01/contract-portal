e6d5b5f93cf410147acdf3d76750d4d9af88c5d0
pkasinski <kasinski@student.chalmers.se>
2022-04-27 15:11:15 +0200

    Add endpoint for validation of users
    Co-authored-by: Mathias Prétot <mathias.pretot@gmail.com>

--- src/Server/Controllers/UsersController.cs
+++ src/Server/Controllers/UsersController.cs
@@ -63,9 +63,9 @@ public class UsersController : BaseApiController<UsersController>
     /// </summary>
     /// <param name="username">The username.</param>
     /// <returns>Whether a user with the specified username exists.</returns>
-    [HttpGet("validate")]
-    public bool Validate(string username)
+    [HttpPost("validate")]
+    public IActionResult Validate(string username)
     {
-        return _users.UserExists(username);
+        return _users.UserExists(username) ? Ok() : BadRequest();
     }
 }
