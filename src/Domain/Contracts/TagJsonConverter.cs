ce1734d263ca06238f0efe38ba37090e6560a8da
Martin <martinjonsson01@gmail.com>
2022-04-27 12:45:15 +0200

    Create json converter

---
+++ src/Domain/Contracts/TagJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Domain.Contracts;
+
+/// <summary>
+/// Converts tags to and from JSON.
+/// </summary>
+public class TagJsonConverter : JsonConverter<Tag>
+{
+    /// <inheritdoc/>
+    public override Tag Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var id = Guid.Parse(reader.GetString() ?? string.Empty);
+        string text = reader.GetString() ?? string.Empty;
+
+        return new Tag { Id = id, Text = text, };
+    }
+
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, Tag value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.Id);
+        writer.WriteStringValue(value.Text);
+    }
+}
