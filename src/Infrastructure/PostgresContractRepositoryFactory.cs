bbf62e3cb82d644e952a27901436b768e444bb27
Martin <martinjonsson01@gmail.com>
2022-04-27 14:13:57 +0200

    Create factory that is used by EF at design time

---
+++ src/Infrastructure/PostgresContractRepositoryFactory.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Contracts;
+
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure;
+
+/// <summary>
+/// Used at design-time by Entity Framework Core to generate the database migrations.
+/// </summary>
+public class PostgresContractRepositoryFactory : IDesignTimeDbContextFactory<PostgresContractRepository>
+{
+    /// <inheritdoc />
+    public PostgresContractRepository CreateDbContext(string[] args)
+    {
+        return new PostgresContractRepository(new LoggerFactory().CreateLogger<PostgresContractRepository>());
+    }
+}
