31ba457ef947ec4df2eaa4ec3f798405f3ab51de
Martin <martinjonsson01@gmail.com>
2022-05-02 17:41:30 +0200

    Make tests run on github execute untrusted

--- src/Infrastructure/EnvironmentVariableKeys.cs
+++ src/Infrastructure/EnvironmentVariableKeys.cs
@@ -6,7 +6,12 @@
 public static class EnvironmentVariableKeys
 {
     /// <summary>
-    /// The name of environment variable.
+    /// The name of the environment variable containing the database connection string.
     /// </summary>
     public const string DbConnectionString = "prodigo_portal_db_connectionstring";
+
+    /// <summary>
+    /// The name of the environment variable containing whether the database connection is untrusted.
+    /// </summary>
+    public const string UntrustedConnection = "UNTRUSTED_CONNECTION";
 }
--- tests/Infrastructure.Tests/TestUserDatabaseFixture.cs
+++ tests/Infrastructure.Tests/TestUserDatabaseFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 using Infrastructure.Users;
 
@@ -10,7 +11,7 @@ namespace Infrastructure.Tests;
 public class TestUserDatabaseFixture
 {
     private const string ConnectionString =
-        @"Server=localhost;Database=master_test_user;Trusted_Connection=True;User Id=SA; Password=ASDjk_shd$$jkASKJ19821";
+        @"Server=localhost;Database=master_test_user;User Id=SA; Password=ASDjk_shd$$jkASKJ19821;";
 
     private static readonly object _lock = new();
     private static bool _databaseInitialized;
@@ -38,9 +39,15 @@ public class TestUserDatabaseFixture
         Justification = "Needs to be non-static so each test instance can call it without the entire class being static.")]
     public EFUserRepository CreateContext()
     {
+        string connectionString = ConnectionString;
+
+        string? untrusted = Environment.GetEnvironmentVariable(EnvironmentVariableKeys.UntrustedConnection);
+        if (untrusted is null)
+            connectionString += "Trusted_Connection=True;";
+
         return new EFUserRepository(
             new DbContextOptionsBuilder<EFUserRepository>()
-                .UseSqlServer(ConnectionString)
+                .UseSqlServer(connectionString)
                 .Options,
             Mock.Of<ILogger<EFUserRepository>>());
     }
