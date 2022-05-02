6f6569ea5ce3fdbfaf1624629266a8ba4d144125
Martin <martinjonsson01@gmail.com>
2022-05-02 15:40:34 +0200

    Change to more complex password

--- tests/Infrastructure.Tests/TestContractDatabaseFixture.cs
+++ tests/Infrastructure.Tests/TestContractDatabaseFixture.cs
@@ -10,7 +10,7 @@ namespace Infrastructure.Tests;
 public class TestContractDatabaseFixture
 {
     private const string ConnectionString =
-        @"Server=localhost;Database=master_test;Trusted_Connection=True;User Id='SA'; Password='Password!'";
+        @"Server=localhost;Database=master_test;Trusted_Connection=True;User Id='SA'; Password='ASDjk_shd$$jkASKJ19821!'";
 
     private static readonly object _lock = new();
     private static bool _databaseInitialized;
