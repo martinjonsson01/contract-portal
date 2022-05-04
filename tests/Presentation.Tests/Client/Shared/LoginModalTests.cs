4023dd4679a5adb965a1f3360a72962c14e31f6e
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-05-04 16:07:07 +0200

    Add password field

--- tests/Presentation.Tests/Client/Shared/LoginModalTests.cs
+++ tests/Presentation.Tests/Client/Shared/LoginModalTests.cs
@@ -1,6 +1,18 @@
-﻿namespace Presentation.Tests.Client.Shared;
+﻿using Client.Shared;
 
-public class LoginModalTests
+namespace Presentation.Tests.Client.Shared;
+
+public class LoginModalTests : UITestFixture
 {
-    
+    [Fact]
+    public void PasswordFieldExists()
+    {
+        // Arrange
+        IRenderedComponent<LoginModal> cut = Context.RenderComponent<LoginModal>();
+        cut.Render();
+
+        // Assert
+        string test = cut.Markup;
+        test.Should().Contain("id=password");
+    }
 }
