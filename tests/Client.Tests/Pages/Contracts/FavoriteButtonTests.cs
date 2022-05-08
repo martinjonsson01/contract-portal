fbc8178166079fce549277f804c94be2644044e9
Jacob Bredin <jacob@bredin.se>
2022-05-08 10:38:19 +0200

    Add FavoriteButton unit tests

--- tests/Client.Tests/Pages/Contracts/FavoriteButtonTests.cs
+++ tests/Client.Tests/Pages/Contracts/FavoriteButtonTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net;
 using System.Net.Http;
+using AngleSharp.Dom;
+using Bunit;
 using Client.Pages.Contracts;
 
 using Domain.Contracts;
@@ -13,7 +15,7 @@ public class FavoriteButtonTests : UITestFixture
     {
         // Arrange
         string userName = "user";
-        var contract = new Contract();
+        Contract contract = new();
 
         void ParameterBuilder(ComponentParameterCollectionBuilder<FavoriteButton> parameters) =>
             parameters.Add(property => property.Contract, contract)
@@ -50,5 +52,84 @@ public class FavoriteButtonTests : UITestFixture
         // Assert
         cut.Find(".bi-heart").Should().NotBeNull();
         Assert.Throws<ElementNotFoundException>(() => cut.Find(".bi-heart-fill"));
+    }
+
+    [Fact]
+    public void FavoriteButton_CallsOnFavoriteChange_WhenClicked()
+    {
+        // Arrange
+        string userName = "user";
+        Contract contract = new();
+
+        bool eventCalled = false;
+
+        MockHttp.When($"/api/v1/favorites").Respond(req => new HttpResponseMessage(HttpStatusCode.OK));
+
+        void ParameterBuilder(ComponentParameterCollectionBuilder<FavoriteButton> parameters) =>
+                      parameters.Add(property => property.Contract, contract)
+                                .Add(property => property.LoggedInUser, userName)
+                                .Add(property => property.OnFavoriteChange, () => { eventCalled = true; });
+
+        IRenderedComponent<FavoriteButton> cut = Context.RenderComponent<FavoriteButton>(ParameterBuilder);
+
+        // Act
+        cut.Find("#Favorite-button").Click();
+
+        // Assert
+        Assert.True(eventCalled);
+    }
+
+    [Fact]
+    public void FavoriteButton_DoesNotChangeAppearance_WhenRequestFailed()
+    {
+        // Arrange
+        string userName = "user";
+        Contract contract = new();
+
+        MockHttp.When($"/api/v1/favorites/{userName}/{contract.Id}").Respond(req => new HttpResponseMessage(HttpStatusCode.OK)); // The contract is marked as favorite
+        MockHttp.When($"/api/v1/favorites").Respond(req => new HttpResponseMessage(HttpStatusCode.BadRequest)); // The contract does not get unmarked
+
+        void ParameterBuilder(ComponentParameterCollectionBuilder<FavoriteButton> parameters) =>
+                      parameters.Add(property => property.Contract, contract)
+                                .Add(property => property.LoggedInUser, userName);
+
+        IRenderedComponent<FavoriteButton> cut = Context.RenderComponent<FavoriteButton>(ParameterBuilder);
+
+        IElement beforeClicked = cut.Find(".bi-heart-fill");
+
+        // Act
+        cut.Find("#Favorite-button").Click();
+        IElement afterClicked = cut.Find(".bi-heart-fill");
+
+        // Assert
+        beforeClicked.Should().NotBeNull();
+        afterClicked.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void FavoriteButton_ChangesAppearance_WhenRequestSucceeds()
+    {
+        // Arrange
+        string userName = "user";
+        Contract contract = new();
+
+        MockHttp.When($"/api/v1/favorites/{userName}/{contract.Id}").Respond(req => new HttpResponseMessage(HttpStatusCode.OK)); // The contract is marked as favorite
+        MockHttp.When($"/api/v1/favorites").Respond(req => new HttpResponseMessage(HttpStatusCode.OK)); // The contract does get unmarked
+
+        void ParameterBuilder(ComponentParameterCollectionBuilder<FavoriteButton> parameters) =>
+                      parameters.Add(property => property.Contract, contract)
+                                .Add(property => property.LoggedInUser, userName);
+
+        IRenderedComponent<FavoriteButton> cut = Context.RenderComponent<FavoriteButton>(ParameterBuilder);
+
+        IElement beforeClicked = cut.Find(".bi-heart-fill");
+
+        // Act
+        cut.Find("#Favorite-button").Click();
+        IElement afterClicked = cut.Find(".bi-heart");
+
+        // Assert
+        beforeClicked.Should().NotBeNull();
+        afterClicked.Should().NotBeNull();
     }
 }
