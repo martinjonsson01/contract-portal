3b167325c926b5e61f77070e15abde846296b65e
Martin <martinjonsson01@gmail.com>
2022-05-11 09:28:53 +0200

    Change token expiry date to 1 month

--- src/Application/Users/UserService.cs
+++ src/Application/Users/UserService.cs
@@ -95,7 +95,7 @@ public class UserService : IUserService
             Subject = new ClaimsIdentity(CreateClaims(user)),
             Audience = _config[ConfigurationKeys.JwtIssuer],
             Issuer = _config[ConfigurationKeys.JwtIssuer],
-            Expires = DateTime.MaxValue,
+            Expires = DateTime.Now.AddMonths(1),
             SigningCredentials =
                 new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
         };
