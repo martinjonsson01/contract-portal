c54674ef40d932efb8e8cbafe64bca2eba2af762
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-05-18 15:51:03 +0200

    Remove double service

--- src/Application/InjectionExtensions.cs
+++ src/Application/InjectionExtensions.cs
@@ -19,7 +19,6 @@ public static class InjectionExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         return services.AddTransient<IContractService, ContractService>()
-                       .AddTransient<IStatusUpdateService, NotificationService>()
                        .AddTransient<IRecentContractService, LimitedRecentContractService>()
                        .AddTransient<IUserService, UserService>()
                        .AddScoped(typeof(SearchEngine<>))
