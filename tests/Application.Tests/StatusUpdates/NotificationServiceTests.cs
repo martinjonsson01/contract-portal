fb06e57b925d4a1869583059ff66fa42b5649788
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-05-18 16:54:12 +0200

    Add noticication service test class

---
+++ tests/Application.Tests/StatusUpdates/NotificationServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Application.StatusUpdates;
+
+namespace Application.Tests.StatusUpdates;
+
+public class NotificationServiceTests
+{
+    private readonly NotificationService _cut;
+    private readonly Mock<IStatusUpdateRepository> _mockRepo;
+
+    public NotificationServiceTests()
+    {
+        _mockRepo = new Mock<IStatusUpdateRepository>();
+        _cut = new NotificationService(_mockRepo.Object);
+    }
+}
