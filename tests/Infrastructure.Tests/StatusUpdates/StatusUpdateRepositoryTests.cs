21edbc399ddbb79994ead6079606233738e458cb
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2022-05-18 16:41:55 +0200

    Add test for add-method

--- tests/Infrastructure.Tests/StatusUpdates/StatusUpdateRepositoryTests.cs
+++ tests/Infrastructure.Tests/StatusUpdates/StatusUpdateRepositoryTests.cs
@@ -1,6 +1,37 @@
-﻿namespace Infrastructure.Tests.StatusUpdates;
+﻿using Application.StatusUpdates;
 
-public class StatusUpdateRepositoryTests
+using Domain.StatusUpdates;
+
+using Infrastructure.Databases;
+
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Tests.StatusUpdates;
+
+public class StatusUpdateRepositoryTests : IClassFixture<TestDatabaseFixture>
 {
-    
+    private readonly TestDatabaseFixture _fixture;
+    private IStatusUpdateRepository _cut;
+
+    public StatusUpdateRepositoryTests(TestDatabaseFixture fixture)
+    {
+        _fixture = fixture;
+        EFDatabaseContext context = _fixture.CreateContext();
+        _cut = new EFStatusUpdatesRepository(context, Mock.Of<ILogger<EFUserRepository>>());
+        context.Database.BeginTransaction();
+    }
+
+    [Fact]
+    public void AddStatusUpdateStoresTheStatusUpdate()
+    {
+        // Arrange
+        var statusUpdate = new StatusUpdate();
+
+        // Act
+        _cut.Add(statusUpdate);
+        IEnumerable<StatusUpdate> statusUpdates = _cut.All;
+
+        // Assert
+        statusUpdate.Should().NotBeNull();
+    }
 }
