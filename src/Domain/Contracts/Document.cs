f4f1395af47f58f06fb28764b9c3ee30c1dd7440
Martin <martinjonsson01@gmail.com>
2022-05-18 07:46:17 +0200

    Create Document entity

---
+++ src/Domain/Contracts/Document.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Contracts;
+
+/// <summary>
+/// Additional information that supplements a <see cref="Contract"/>.
+/// </summary>
+public class Document
+{
+    /// <summary>
+    /// Gets or sets the identifier.
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Gets or sets where to find the document.
+    /// </summary>
+    public string Path { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+}
