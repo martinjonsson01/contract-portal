d8da7448aa74cee9a757a44e213d6ab8ee0ebd8a
XwayTomy <Tamas_nagy@hotmail.com>
2022-05-22 13:39:19 +0200

    Delete redundant file
    Co-authored-by: Ella Sibbmark <ella@sibbmark.se>

---
+++ tests/Client.Tests/Pages/Admin/NotificationListItemTests.cs
@@ -0,0 +1,20 @@
+﻿using Client.Pages.Admin.NotificationComponents;
+
+using Domain.StatusUpdates;
+
+namespace Client.Tests.Pages.Admin;
+
+public class NotificationListItemTests : IDisposable
+{
+    private readonly TestContext _context;
+
+    public NotificationListItemTests()
+    {
+        _context = new TestContext();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
